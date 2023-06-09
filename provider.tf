81d40c7e2504cb489184f95cd353e5155869ab63
timoph2 <98256868+timoph2@users.noreply.github.com>
2023-06-09 13:53:38 +0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,12 @@
 provider "aws" {
   region = var.region
-  access_key = "AKIA45WHSWTDR3P4S5VA"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
 # terraform {
 #   backend "s3" {
 #     bucket = "terraform-iac-state"
-#     key = "terraform.tfstate"
 #     region = "ap-southeast-1"
 #   }
 # }
